eaec7c4f12a2f95c5080aa55e454840ac5d2d2c1
Hoan <lenguyenhanhoan@gmail.com>
2019-11-19 00:00:40 +0700

    update conection

--- includes/connect.php
+++ includes/connect.php
@@ -5,12 +5,21 @@
 // $db = "itec2018week8";
 // $conn = new mysqli($host, $user, $pw, $db);
 
-$host = "remotemysql.com";
-$user = "pzmyS9yKiP";
-$pw = "N6id2BhU0L";
-$db = "pzmyS9yKiP";
-$conn = new mysqli($host, $user, $pw, $db);
+// $host = "hoanle.database.windows.net";
+// $user = "pzmyS9yKiP";
+// $pw = "N6id2BhU0L";
+// $db = "pzmyS9yKiP";
+// $conn = new mysqli($host, $user, $pw, $db);
 
+
+$serverName = "hoanle.database.windows.net"; // update me
+$connectionOptions = array(
+    "Database" => "HoanDb", // update me
+    "Uid" => "hoanle", // update me
+    "PWD" => "$123qwe$" // update me
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
 if ($conn->connect_error) {
     echo "Connection failed: " . $conn->connect_error;
 } else {
