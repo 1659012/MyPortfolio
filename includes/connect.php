a2cc165189fa509543d75d86b0cec0e7a209b332
Hoan <lenguyenhanhoan@gmail.com>
2019-11-19 00:10:54 +0700

    fix

--- includes/connect.php
+++ includes/connect.php
@@ -5,23 +5,23 @@
 // $db = "itec2018week8";
 // $conn = new mysqli($host, $user, $pw, $db);
 
-// $host = "hoanle.database.windows.net";
-// $user = "pzmyS9yKiP";
-// $pw = "N6id2BhU0L";
-// $db = "pzmyS9yKiP";
-// $conn = new mysqli($host, $user, $pw, $db);
+$host = "hoanle.database.windows.net";
+$user = "pzmyS9yKiP";
+$pw = "N6id2BhU0L";
+$db = "pzmyS9yKiP";
+$conn = new mysqli($host, $user, $pw, $db);
 
 
-$serverName = "hoanle.database.windows.net"; // update me
-$connectionOptions = array(
-    "Database" => "HoanDb", // update me
-    "Uid" => "hoanle", // update me
-    "PWD" => "$123qwe$" // update me
-);
-//Establishes the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-if ($conn) {
-    echo "Connected successfully" . "<br>";   
-} else {
-    echo "Connection could not be established.<br />";
-}
+// $serverName = "hoanle.database.windows.net"; // update me
+// $connectionOptions = array(
+//     "Database" => "HoanDb", // update me
+//     "Uid" => "hoanle", // update me
+//     "PWD" => "$123qwe$" // update me
+// );
+// Establishes the connection
+// $conn = sqlsrv_connect($serverName, $connectionOptions);
+// if ($conn) {
+//     echo "Connected successfully" . "<br>";   
+// } else {
+//     echo "Connection could not be established.<br />";
+// }
